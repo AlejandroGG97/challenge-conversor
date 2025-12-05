c6581239ccf7c32f06b9f120444aafdd9a41a800
Alejandro Guzman <alejandrogg9751@gmail.com>
2025-12-05 16:42:48 -0600

    emepezando

--- conversor-divisas/src/peticion/datos/Peticion.java
+++ conversor-divisas/src/peticion/datos/Peticion.java
@@ -1,4 +1,24 @@
 package peticion.datos;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Peticion {
+
+    String direccion = "https://v6.exchangerate-api.com/v6/4068694d72e3b8614c4e06fc/latest/USD";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    try{
+
+    } catch (IOException e){}
+
+
 }
